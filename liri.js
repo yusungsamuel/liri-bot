78d9fd9da6888db04de23a61998c51607a6b5dee
yusungsamuel <ssyyu@ucdavis.edu>
2019-03-19 15:04:48 -0700

    add prompt for when app is initiated

--- liri.js
+++ liri.js
@@ -8,3 +8,27 @@ var axios = require("axios")
 
 var inquirer = require("inquirer")
 
+function initiate () {
+    inquirer.prompt([
+        {
+            type: "list",
+            message: "What can I do for you?",
+            name: "option",
+            choices: ["concert-this", "spotify-this-song", "movie-this", "do-what-it-says"]
+        }
+    ]).then(function(response){
+        if (response.option === "concert-this"){
+
+        }
+        else if (response.option === "spotify-this-song"){
+            
+        }
+        else if (response.option === "movie-this"){
+            
+        }
+        else if (response.option === "do-what-it-says"){
+            
+        }
+    }) 
+}
+
